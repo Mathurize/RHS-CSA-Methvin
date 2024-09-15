7732a4a4f99cc8efda1d6fa708d5d2448b7f82c7
James Methvin <jmethvin@lwsd.org>
2024-09-15 12:05:12 -0700

    Add starter code for lesson 4 activities: Warm-up, Figures Decomposition, and Extra Practice.

---
+++ Chapter 1 - Intro to Java/Lesson4/FigureDecomposition/Figures1.java
@@ -0,0 +1,25 @@
+public class Figures1 {
+    public static void main(String[] args) {
+        System.out.println("  ______");
+        System.out.println(" /      \\");
+        System.out.println("/        \\");
+        System.out.println("\\        /");
+        System.out.println(" \\______/");
+        System.out.println();
+        System.out.println("\\        /");
+        System.out.println(" \\______/");
+        System.out.println("+--------+");
+        System.out.println();
+        System.out.println("  ______");
+        System.out.println(" /      \\");
+        System.out.println("/        \\");
+        System.out.println("|  STOP  |");
+        System.out.println("\\        /");
+        System.out.println(" \\______/");
+        System.out.println();
+        System.out.println("  ______");
+        System.out.println(" /      \\");
+        System.out.println("/        \\");
+        System.out.println("+--------+");
+    }
+}
---
+++ Chapter 1 - Intro to Java/Lesson4/Warm-up/AppMain.java
@@ -0,0 +1,8 @@
+public class AppMain {
+    Comment out this message
+     Use the multi-line comment style for this one.
+    public static void main(String[] args) {
+        Here's the print command (comment this out as well, using the single line style).
+        System.out.println(my text);
+    }
+}
