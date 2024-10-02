05c45b874f59369ea83a943cf9fd204919203e20
James Methvin <jmethvin@lwsd.org>
2024-10-02 11:48:29 -0700

    Add BasicTests.java file. Will flesh out tests next.

---
+++ Chapter 2 - Variables and Loops/HourglassASCIIArt/test/BasicTests.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.*;
+
+import java.lang.reflect.Field;
+
+public class BasicTests {
+    @Test
+    public void testSanity() {
+        assert(true);
+    }
+
+    // Console output helper methods
+    private ByteArrayOutputStream consoleStringOutput = null;
+    private PrintStream consoleStringOutputPrev = null;
+    private void consoleToString_Push() {
+        if (consoleStringOutput != null) {
+            consoleToString_Pop();
+        }
+
+        // Stream to hold the output
+        consoleStringOutput = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(consoleStringOutput);
+
+        // Save the old system out
+        consoleStringOutputPrev = System.out;
+
+        // Set the new output
+        System.setOut(ps);
+    }
+    private String consoleToString_Pop() {
+        // restore
+        System.out.flush();
+        System.setOut(consoleStringOutputPrev);
+
+        return consoleStringOutput.toString();
+    }
+    private String runMethodGetConsoleOut(Runnable method) {
+        consoleToString_Push();
+        method.run();
+        return consoleToString_Pop();
+    }    
+}
