7e3fcf51df88dd54eadbeb71e6fe13b151e99498
James Methvin <jmethvin@lwsd.org>
2024-10-02 11:14:51 -0700

    Check in starter code for Hourglass ASCII Art Lab Assignment

---
+++ Chapter 2 - Variables and Loops/HourglassASCIIArt/HourGlass.java
@@ -0,0 +1,19 @@
+/*
+ * Example output for Hourglass size 3
+ * 
+ * +------+
+ * |\..../|
+ * | \../ |
+ * |  \/  |
+ * |  /\  |
+ * | /..\ |
+ * |/....\|
+ * +------+ 
+ * 
+ * NOTE: See Hourglass ASCII Art Assignment Handout for further details
+ */
+public class HourGlass {
+    public static void main(String[] args) {
+        // TODO: Implement Hourglass ASCII Art Program as instructed in activity guide.
+    }
+}
