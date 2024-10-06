88619bd5ce51c27dec1ac14225ea045788b55742
James Methvin <jmethvin@lwsd.org>
2024-10-06 13:44:19 -0700

    Add starter code for lesson 4 activity - Noble Name

---
+++ Chapter 3 - Methods/L4/NobleName.java
@@ -0,0 +1,19 @@
+// TODO: Need any imports?
+// TODO: Create a file/class header describing the purpose of this program
+
+/* TODO: Write a program that outputs a person’s “noble name"
+ * Prime Minister
+ * first name
+ * first initial
+ * last name (all caps)
+ * , esquire
+ * , of Durdle Door
+
+ * Example Output:
+ * Type your name, noble: Marge Simpson
+ * Your noble name is “Prime Minister Marge M. SIMPSON, esquire, of Durdle Door"
+
+ */
+
+// TODO: Create a class called NobleName
+// TODO: Create a main method within the NobleName class
