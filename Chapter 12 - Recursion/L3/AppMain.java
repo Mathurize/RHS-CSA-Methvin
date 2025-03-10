def7e29f7aa5e400665bf4b224f7653275856230
James Methvin <jmethvin@lwsd.org>
2025-03-09 20:40:26 -0700

    Checking in files for the Lesson 3 slides. NOTE: There is some overlap with existing labs in this directory path, but I'd rather duplicate some and avoid merge problems in the future.

---
+++ Chapter 12 - Recursion/L3/AppMain.java
@@ -0,0 +1,23 @@
+public class AppMain {
+    public static void main(String[] args) {
+        Practice practice = new Practice();
+
+        System.out.println("-------- sumFibonacciSeq --------");
+        // TODO: add your test cases here
+
+        System.out.println("-------- reverseStr --------");
+        // TODO: add your test cases here
+
+        System.out.println("-------- sumEvens --------");
+        // TODO: add your test cases here
+
+        System.out.println("-------- countChar --------");
+        // TODO: add your test cases here
+
+        System.out.println("\n-------- createStringR --------");
+        // TODO: add your test cases here
+
+        System.out.println("\n-------- printBinary --------");
+        // TODO: add your test cases here
+    }
+}
