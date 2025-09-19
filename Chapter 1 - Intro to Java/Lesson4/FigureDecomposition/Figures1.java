d0ea1059209fe45043684cb64ead2acf0a7e52d6
Ruhaan <1057621@lwsd.org>
2025-09-19 16:00:52 -0700

    Lesson 4 decomposition

--- Chapter 1 - Intro to Java/Lesson4/FigureDecomposition/Figures1.java
+++ Chapter 1 - Intro to Java/Lesson4/FigureDecomposition/Figures1.java
@@ -1,25 +1,40 @@
 public class Figures1 {
     public static void main(String[] args) {
-        System.out.println("  ______");
-        System.out.println(" /      \\");
-        System.out.println("/        \\");
-        System.out.println("\\        /");
-        System.out.println(" \\______/");
-        System.out.println();
-        System.out.println("\\        /");
-        System.out.println(" \\______/");
-        System.out.println("+--------+");
-        System.out.println();
-        System.out.println("  ______");
-        System.out.println(" /      \\");
-        System.out.println("/        \\");
-        System.out.println("|  STOP  |");
-        System.out.println("\\        /");
-        System.out.println(" \\______/");
-        System.out.println();
-        System.out.println("  ______");
-        System.out.println(" /      \\");
-        System.out.println("/        \\");
-        System.out.println("+--------+");
+        egg();
+        teaCup();
     }
+
+
+public static void egg(){
+    System.out.println("  ______");
+    System.out.println(" /      \\");
+    System.out.println("/        \\");
+    System.out.println("\\        /");
+    System.out.println(" \\______/");
+    System.out.println();
+}
+
+public static void teaCup(){
+    System.out.println("\\        /");
+    System.out.println(" \\______/");
+    System.out.println("+--------+");
+    System.out.println();
+}
+
+public static void stopSign(String[] args){
+System.out.println("  ______");
+    System.out.println(" /      \\");
+    System.out.println("/        \\");
+    System.out.println("|  STOP  |");
+    System.out.println("\\        /");
+    System.out.println(" \\______/");
+    System.out.println();
+}
+public static void top(String[] args){
+    System.out.println("  ______");
+    System.out.println(" /      \\");
+    System.out.println("/        \\");
+    System.out.println("+--------+");
+}
+
 }
